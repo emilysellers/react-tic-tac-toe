942a9a8bc4f249fc8a37aa590c5fca79ff59430d
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-27 14:23:48 -0800

    refactor so setGameBoard updates gameBoard state

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,7 +3,8 @@ import { useGameContext } from '../context/GameContext.js';
 import './Box.css';
 
 export default function Box({ box }) {
-  const { active, setCurrentPlayer, currentPlayer, setGameMessage } = useGameContext();
+  const { active, setCurrentPlayer, currentPlayer, setGameMessage, gameBoard, setGameBoard } =
+    useGameContext();
 
   const handleClick = () => {
     // If the space already contains an ‘X’ or an ‘O’, return
@@ -18,6 +19,9 @@ export default function Box({ box }) {
     // Otherwise, set the space’s content to the current player
     else {
       box.content = currentPlayer;
+      const newBoard = [...gameBoard];
+      setGameBoard(newBoard);
+
       //   console.log(index);
       //   console.log(gameBoard[index].content);
     }

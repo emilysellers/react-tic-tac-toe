d367517129bc39aa85f43b88312c957d048fc02e
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-26 16:11:50 -0800

    map boxes to render 9 boxes on board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useGameContext } from '../context/GameContext.js';
 import Box from './Box.js';
 
 export default function Board() {
+  const { boxes } = useGameContext();
   return (
     <div>
       <h1>Tic Tac Toe!</h1>
-      <Box />
+      <div>
+        {boxes.map((box) => (
+          <Box key={box.space} />
+        ))}
+      </div>
     </div>
   );
 }
